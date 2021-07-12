52c01c277b2698dfb2e75b7e42e30d6a53360e40
Wiktor Nowak <wnowak2f@gmail.com>
2021-07-12 14:52:47 +0200

    Relations between operations, user and bank bank statement tables

--- backend/bank_statement/models.py
+++ backend/bank_statement/models.py
@@ -6,5 +6,5 @@ from backend.operations.models import Operations
 
 class BankStatement(models.Model):
     notes = models.CharField(max_length=200)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
     operations = models.ForeignKey(Operations, null=True, on_delete=models.CASCADE)
