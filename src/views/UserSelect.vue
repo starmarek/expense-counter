d2f23d34e9464e2209df19c33b78d1bf40438428
Aleksander <pucher.aleksander@gmail.com>
2021-08-03 10:37:10 +0200

    add addUser button

--- src/views/UserSelect.vue
+++ src/views/UserSelect.vue
@@ -1,20 +1,30 @@
 <template>
-    <div
-        style="height: 100vh; background-color: Grey"
-        class="is-flex is-align-items-center is-justify-content-space-evenly"
-    >
-        <div
-            class="is-flex is-flex-direction-column is-align-items-center"
-            v-for="user in users"
-            :key="user.id"
+    <div>
+        <b-button
+            rounded
+            class="m-5 add-button"
+            size="is-medium"
+            icon-left="plus"
+            @click="addUserModalActive = true"
+            >Add user</b-button
         >
-            <figure class="image is-128x128">
-                <img class="is-rounded" src="@/assets/user_logo.png" />
-            </figure>
-            <p style="color: black" class="title">
-                {{ user.first_name }} {{ user.last_name }}
-            </p>
-            <b-button @click="chooseUser(user)">Choose</b-button>
+        <div
+            style="height: 100vh; background-color: lightgrey"
+            class="is-flex is-align-items-center is-justify-content-space-evenly"
+        >
+            <div
+                class="is-flex is-flex-direction-column is-align-items-center"
+                v-for="user in users"
+                :key="user.id"
+            >
+                <figure class="image is-128x128">
+                    <img class="is-rounded" src="@/assets/user_logo.png" />
+                </figure>
+                <p style="color: black" class="title">
+                    {{ user.first_name }} {{ user.last_name }}
+                </p>
+                <b-button size="is-medium" @click="chooseUser(user)">Choose</b-button>
+            </div>
         </div>
     </div>
 </template>
@@ -23,6 +33,11 @@
 import { mapState, mapMutations } from "vuex";
 export default {
     name: "UserSelect",
+    data() {
+        return {
+            addUserModalActive: false,
+        };
+    },
     methods: {
         ...mapMutations("user", ["setChosenUser"]),
         chooseUser(user) {
@@ -40,3 +55,11 @@ export default {
     },
 };
 </script>
+<style scoped>
+.add-button {
+    position: fixed;
+    bottom: 0;
+    color: white;
+    background-color: #566ac2;
+}
+</style>
