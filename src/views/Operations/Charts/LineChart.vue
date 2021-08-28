0e8f5748a336cbea7544de09326af0d36fc531b3
Wiktor Nowak <wnowak2f@gmail.com>
2021-08-28 20:17:04 +0200

    Basic statistics frontend with local data plot and current user in history

---
+++ src/views/Operations/Charts/LineChart.vue
@@ -0,0 +1,19 @@
+<script>
+import { Line } from "vue-chartjs";
+export default {
+    extends: Line,
+    props: {
+        chartData: {
+            type: Object,
+            default: null,
+        },
+        options: {
+            responsive: false,
+            maintainAspectRatio: false,
+        },
+    },
+    mounted() {
+        this.renderChart(this.chartData, this.options);
+    },
+};
+</script>
