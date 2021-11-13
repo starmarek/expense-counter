95aed6f66cb68c31024b3bfc21213e8d4ad1c96d
Aleksander Pucher <pucher.aleksander@gmail.com>
2021-11-13 17:16:20 +0100

    wrap userAddModal in form tag to handle enter keyup event

--- src/components/user/UserAddModal.vue
+++ src/components/user/UserAddModal.vue
@@ -1,38 +1,36 @@
 <template>
     <div class="modal-card">
         <ValidationObserver ref="observer" v-slot="{ handleSubmit }">
-            <header class="modal-card-head">
-                <p class="modal-card-title">Create new user</p>
-            </header>
-            <section class="modal-card-body" style="text-align: left">
-                <ValidatedBInput
-                    rules="required|backend-val:auth,User,username"
-                    :debounce="500"
-                    type="text"
-                    label="Username"
-                    v-model="username"
-                ></ValidatedBInput>
-                <ValidatedBInput
-                    rules="required"
-                    type="text"
-                    label="First Name"
-                    v-model="firstName"
-                ></ValidatedBInput
-                ><ValidatedBInput
-                    rules="required"
-                    type="text"
-                    label="Last Name"
-                    v-model="lastName"
-                ></ValidatedBInput>
-            </section>
-            <footer class="modal-card-foot">
-                <b-button label="Close" @click="$parent.close()" />
-                <b-button
-                    label="Create"
-                    @click="handleSubmit(submit)"
-                    type="is-primary"
-                />
-            </footer>
+            <form @submit.prevent="handleSubmit(submit)">
+                <header class="modal-card-head">
+                    <p class="modal-card-title">Create new user</p>
+                </header>
+                <section class="modal-card-body" style="text-align: left">
+                    <ValidatedBInput
+                        rules="required|backend-val:auth,User,username"
+                        :debounce="500"
+                        type="text"
+                        label="Username"
+                        v-model="username"
+                    ></ValidatedBInput>
+                    <ValidatedBInput
+                        rules="required"
+                        type="text"
+                        label="First Name"
+                        v-model="firstName"
+                    ></ValidatedBInput
+                    ><ValidatedBInput
+                        rules="required"
+                        type="text"
+                        label="Last Name"
+                        v-model="lastName"
+                    ></ValidatedBInput>
+                </section>
+                <footer class="modal-card-foot">
+                    <b-button label="Close" @click="$parent.close()" />
+                    <b-button label="Create" type="is-primary" native-type="submit" />
+                </footer>
+            </form>
         </ValidationObserver>
     </div>
 </template>
